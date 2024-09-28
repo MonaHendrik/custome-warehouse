58045df736b235d95dce51a35e521b0a0dc06e13
MonaHendrik <hendrik.terwort@mona-ai.de>
2024-09-29 01:20:22 +0200

    Revert "ada"
    This reverts commit 8ad9f78f7b443031731c997cd5716bbe6d5d007e.

--- costume-warehouse/src/environments/environment.prod.ts
+++ costume-warehouse/src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyAQ2wvgNv8HNziTEcUM0V-2vMJT984BkZY",
-    authDomain: "costume-warehouse-f1a9c.firebaseapp.com",
-    projectId: "costume-warehouse-f1a9c",
-    storageBucket: "costume-warehouse-f1a9c.appspot.com",
-    messagingSenderId: "696411134970",
-    appId: "1:696411134970:web:6ed8b72a11a8bf355de243"
-  },
+  production: true
 };
