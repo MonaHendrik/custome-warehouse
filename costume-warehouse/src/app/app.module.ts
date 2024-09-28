fe3bf04330da4ccc36b3bb714ac52082b486b43b
MonaHendrik <hendrik.terwort@mona-ai.de>
2024-09-29 01:15:52 +0200

    Revert "added Firebase"
    This reverts commit 88c069decc597115623a192f18117ac43df5cd01.

--- costume-warehouse/src/app/app.module.ts
+++ costume-warehouse/src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirestoreModule } from '@angular/fire/firestore';
-import { AngularFireModule} from '@angular/fire/compat'
-import { environment } from '../environments/environment';
-import { RouteReuseStrategy } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    FirestoreModule
-
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- costume-warehouse/src/environments/environment.ts
+++ costume-warehouse/src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyAQ2wvgNv8HNziTEcUM0V-2vMJT984BkZY",
-    authDomain: "costume-warehouse-f1a9c.firebaseapp.com",
-    projectId: "costume-warehouse-f1a9c",
-    storageBucket: "costume-warehouse-f1a9c.appspot.com",
-    messagingSenderId: "696411134970",
-    appId: "1:696411134970:web:6ed8b72a11a8bf355de243"
-  },
+  production: false
 };
 
 /*
