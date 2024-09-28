488a434c85d3ae598436183188e4e226149de982
MonaHendrik <hendrik.terwort@mona-ai.de>
2024-09-29 01:30:24 +0200

    adadaad

--- costume-warehouse/src/environments/environment.prod.ts
+++ costume-warehouse/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAQ2wvgNv8HNziTEcUM0V-2vMJT984BkZY",
+    authDomain: "costume-warehouse-f1a9c.firebaseapp.com",
+    projectId: "costume-warehouse-f1a9c",
+    storageBucket: "costume-warehouse-f1a9c.appspot.com",
+    messagingSenderId: "696411134970",
+    appId: "1:696411134970:web:6ed8b72a11a8bf355de243"
+  }
 };
